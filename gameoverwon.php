cee89110851fe8ac44bc811c44daef0bb34eac92
adamhankins <adamhankins@outlook.com>
2016-01-31 12:10:08 -0500

    Create gameoverwon.php

---
+++ gameoverwon.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Haunted Hamburger</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="title">Haunted Hamburger</div><br>
+        <div id="gameWonMessage" class="message"><p>You made it out of the Haunted Hamburger!
+            As soon as you bring the Magic Spatula outside, the Meat Monster
+            falls to the ground into a heap of hamburger! You can now go
+            and collect your payday from the owner! Congratulations!</p>
+            <button id="playAgainButton">Play Again</button>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="javascript/startscript.js"></script>
+    </body>
+</html>
