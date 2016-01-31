83e91111b3fcba97a81e839b6ce19d4ed77b9d18
adamhankins <adamhankins@outlook.com>
2016-01-31 12:09:14 -0500

    Create gameoverlost.php

---
+++ gameoverlost.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Haunted Hamburger</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="title">Haunted Hamburger</div><br>
+        <div id="gameLostMessage" class="message"><p>The Meat Monster attacks you and eats you!
+                Your adventure in the Haunted Hamburger has come to a gruesome
+                end!</p>
+            <button id="playAgainButton">Play Again</button>
+        </div>        
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="javascript/startscript.js"></script>
+    </body>
+</html>
