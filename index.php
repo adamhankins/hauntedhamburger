e3560d5f8c5240b1ddd65f873898f2491003f9e8
adamhankins <adamhankins@outlook.com>
2016-01-31 12:11:46 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Haunted Hamburger</title>
+        <link rel="stylesheet" type="text/css" href="style.css">
+    </head>
+    <body>
+        <div id="title">Haunted Hamburger</div><br>
+        <div id="game description" class="message"><p>You have been hired by the
+            owner of the
+            Haunted Hamburger to go on a mission to retrieve something very 
+            valuable: the Magic Spatula. It is cooking utensil like no other: 
+            it has the power to bring hamburgers to life--literally! The former
+            management of the Haunted Hamburger accidently spawned something
+            horrendous inside the restaurant, and now it is too dangerous for 
+            them to go back inside. Instead, they have hired you for the dangerous
+            mission and you need the money too badly to back down from the
+            challenge!<br><br>
+            
+            Your instructions: Find the Magic Spatula and leave the restaurant
+            as soon as you get it. The owner has told you that as soon as you're
+            outside the restaurant with the spatula, anything inside will no 
+            longer have any power.<br><br>
+            
+            Use two word commands such as "look tree" or "get chainsaw" to control
+            your character. Click on the directional keypad to move around the 
+            Haunted Hamburger.<br><br>
+            
+            Good luck!</p><br>
+            <button id="startGame">Start Game!</button>
+        </div>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+        <script src="javascript/startscript.js"></script>
+        
+    </body>
+</html>
