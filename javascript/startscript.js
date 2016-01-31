8a7b9952f6a44a1c4719cec27433fc198728840e
adamhankins <adamhankins@outlook.com>
2016-01-31 12:18:15 -0500

    Create startscript.js

---
+++ javascript/startscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+    $('#startGame').on('click', function() {
+        window.location.href = "hauntedhamburger.php";
+    });
+    $('#playAgainButton').on('click', function() {
+        window.location.href = "hauntedhamburger.php";
+    });
+});
